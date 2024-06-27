c41c13a0c7e24086dfda7eb44b495ed83f82fd0b
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-06-27 01:49:08 -0400

    added header

--- flex/src/components/Header.js
+++ flex/src/components/Header.js
@@ -1,7 +1,27 @@
-export default function Header() {
-    return(
-        <div>
-            <h1>This is the header!</h1>
-        </div>
-    );
-}
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <Navbar className="main-nav navbar navbar-expand-lg" expand="lg">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">Flexify</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNav" />
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="ms-auto nav-list">
+            <Nav.Link as={Link} to="/aboutus" className="nav-link-custom">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/classes" className="nav-link-custom">Classes</Nav.Link>
+            <Nav.Link as={Link} to="/instructors" className="nav-link-custom">Instructors</Nav.Link>
+            <Nav.Link as={Link} to="/library" className="nav-link-custom">Library</Nav.Link>
+            <Nav.Link as={Link} to="/reserve" className="nav-link-custom">Reserve</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
