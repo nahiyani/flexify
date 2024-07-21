980e4141d1b72c3d433b4df76196ceeb00603b39
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-07-21 14:07:09 -0400

    Initial commit

--- flex/src/App.js
+++ flex/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs'; 
 import Classes from './pages/Classes';
@@ -10,16 +10,14 @@ import Reserve from './pages/Reserve';
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<AboutUs />} /> 
-          <Route path='/classes' element={<Classes />} />
-          <Route path='/instructors' element={<Instructors />} />
-          <Route path='/library' element={<Library />} />
-          <Route path='/reserve' element={<Reserve />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} /> 
+        <Route path='/classes' element={<Classes />} />
+        <Route path='/instructors' element={<Instructors />} />
+        <Route path='/library' element={<Library />} />
+        <Route path='/reserve' element={<Reserve />} />
+      </Routes>
     </div>
   );
 }
